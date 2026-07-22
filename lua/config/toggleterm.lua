e04dc7a263bbc5da4f2e669378c07b66cbb77407
kazuki-official <kazukiofficial1003@gmail.com>
2026-07-23 08:30:38 +0900

    <Space>Tをnormalモードのみで有効になるように変更

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,3 +1,9 @@
 require("toggleterm").setup{
   open_mapping = [[<Space>T]],
+  insert_mappings = false,
+  terminal_mappings = false,
+  direction = 'float',
+  float_opts = {
+    border = 'curved',
+  },
 }
